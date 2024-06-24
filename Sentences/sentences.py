f84b840cdb625c63bf11f9cc0ecbd5f39bba7d75
sfisherschool <s.fishy97@gmail.com>
2024-06-23 22:46:10 -0400

    Add files via upload

---
+++ Sentences/sentences.py
@@ -0,0 +1,21 @@
+import re
+
+def count_sentences(text):
+    # Pattern to match sentences
+    # (This had to be modified from what was in section 7.4 to properly display the full sentences
+    # and account for sentences that start with a number or non-capital letter)
+    pat = r'[^.!?]*[.!?]'
+
+    # Find all sentences in the text
+    sentences = re.findall(pat, text, flags=re.DOTALL | re.MULTILINE)
+
+    # Print each sentence
+    for i in sentences:
+        print('->', i)
+
+    # Print the total number of sentences
+    print(f'Total number of sentences: {len(sentences)}')
+
+text = input("Enter sentences here: ")
+
+count_sentences(text)
